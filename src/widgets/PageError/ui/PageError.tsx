0572b06065bf22be9ca98b9c404e4fe3a4b3e658
s.markov <s.markov@vipservice.ru>
2022-10-05 08:38:26 +0300

    3.22 ErrorBoundary. Обработка React ошибок

---
+++ src/widgets/PageError/ui/PageError.tsx
@@ -0,0 +1,28 @@
+import { useTranslation } from 'react-i18next';
+import { classNames } from 'shared/lib/classNames/classNames';
+import Button from 'shared/ui/Button/Button';
+import cls from './PageError.module.scss';
+
+interface LangSwitcherProps {
+    className?: string;
+}
+
+export const PageError = ({ className }: LangSwitcherProps) => {
+    const { t } = useTranslation();
+
+    const relaoadPage = () => {
+        // eslint-disable-next-line no-restricted-globals
+        location.reload();
+    };
+
+    return (
+        <div className={classNames(cls.PageError, {}, [className])}>
+            <p>{t('Произошла непредвиденная ошибка')}</p>
+            <Button
+                onClick={relaoadPage}
+            >
+                {t('Перезагрузить страницу')}
+            </Button>
+        </div>
+    );
+};
