314bd37ea9b7209feb22b0f53b9f13b0880b0940
s.markov <s.markov@vipservice.ru>
2023-02-09 20:13:16 +0300

    fix tests

--- src/widgets/Sidebar/model/items.tsx
+++ src/widgets/Sidebar/model/items.tsx
@@ -2,7 +2,7 @@ import { RoutePath } from 'shared/config/routeConfig/routeConfig';
 import MainIcon from 'shared/assets/icons/main-20-20.svg';
 import AboutIcon from 'shared/assets/icons/about-20-20.svg';
 import ProfileIcon from 'shared/assets/icons/profile-20-20.svg';
-import React from 'React';
+import React from 'react';
 
 export interface SidebarItemType {
     path: string;
