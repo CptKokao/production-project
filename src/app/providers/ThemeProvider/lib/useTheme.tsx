778a11ec53f7b9c23ba03958ccac7544e6c3111f
s.markov <s.markov@vipservice.ru>
2023-03-14 21:00:16 +0300

    6.44 Color pallete. Внедряем новую третью тему

--- src/app/providers/ThemeProvider/lib/useTheme.tsx
+++ src/app/providers/ThemeProvider/lib/useTheme.tsx
@@ -10,7 +10,22 @@ export const useTheme = (): useThemeProps => {
     const { theme, setTheme } = useContext(ThemeContext);
 
     const toogleTheme = () => {
-        const newTheme = theme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT;
+        let newTheme: Theme;
+
+        switch (theme) {
+        case Theme.LIGHT:
+            newTheme = Theme.DARK;
+            break;
+        case Theme.DARK:
+            newTheme = Theme.ORANGE;
+            break;
+        case Theme.ORANGE:
+            newTheme = Theme.LIGHT;
+            break;
+        default:
+            newTheme = Theme.LIGHT;
+        }
+
         setTheme?.(newTheme);
         localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme);
     };
