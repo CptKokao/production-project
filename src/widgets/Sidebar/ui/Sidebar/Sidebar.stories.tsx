80a942a20d220f09b1b22bb6957c129fe0831767
s.markov <s.markov@vipservice.ru>
2023-03-14 11:12:05 +0300

    update screenshot

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.stories.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.stories.tsx
@@ -1,5 +1,6 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { Theme } from 'app/providers/ThemeProvider';
+import { storyDecorator } from 'shared/config/storybook/storeDecorator/storeDecorator';
 import { themeDecorator } from 'shared/config/storybook/themeDecorator/themeDecorator';
 import { Sidebar } from './Sidebar';
 
@@ -15,7 +16,26 @@ const Template: ComponentStory<typeof Sidebar> = (args) => <Sidebar {...args} />
 
 export const Light = Template.bind({});
 Light.args = {};
+Light.decorators = [
+    storyDecorator({
+        user: { authData: {} },
+    }),
+];
 
 export const Dark = Template.bind({});
 Dark.args = {};
-Dark.decorators = [themeDecorator(Theme.DARK)];
+Dark.decorators = [
+    themeDecorator(Theme.DARK),
+    storyDecorator({
+        user: { authData: {} },
+    }),
+];
+
+export const NoAuth = Template.bind({});
+NoAuth.args = {};
+NoAuth.decorators = [
+    themeDecorator(Theme.DARK),
+    storyDecorator({
+        user: { },
+    }),
+];
