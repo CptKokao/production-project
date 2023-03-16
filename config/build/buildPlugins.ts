177fb7b2d53364bdc8897175f1b55779ba349c81
s.markov <s.markov@vipservice.ru>
2023-03-16 11:58:22 +0300

    6.46 React refresh plugin. build babel loader

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -7,24 +7,30 @@ import { BuildOptions } from './types/config';
 
 export const buildPlugins = ({
     paths, isDev, project, apiUrl,
-}: BuildOptions):webpack.WebpackPluginInstance[] => [
-    new HtmlWebpackPlugin({
-        template: paths.html,
-    }),
-    new webpack.ProgressPlugin(),
-    new MiniCssExtractPlugin({
-        // filename: 'css/'
-        filename: 'css/[name].[contenthash:8].css',
-        chunkFilename: 'css/[name].[contenthash:8].css',
-    }),
-    new webpack.DefinePlugin({
-        __IS_DEV__: JSON.stringify(isDev),
-        __API_URL__: JSON.stringify(apiUrl),
-        __PROJECT__: JSON.stringify(project),
-    }),
-    new webpack.HotModuleReplacementPlugin(),
-    new ReactRefreshWebpackPlugin(),
-    // new BundleAnalyzerPlugin({
-    //     openAnalyzer: false,
-    // }),
-];
+}: BuildOptions): webpack.WebpackPluginInstance[] => {
+    const plugins = [
+        new HtmlWebpackPlugin({
+            template: paths.html,
+        }),
+        new webpack.ProgressPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].[contenthash:8].css',
+            chunkFilename: 'css/[name].[contenthash:8].css',
+        }),
+        new webpack.DefinePlugin({
+            __IS_DEV__: JSON.stringify(isDev),
+            __API_URL__: JSON.stringify(apiUrl),
+            __PROJECT__: JSON.stringify(project),
+        }),
+    ];
+
+    if (isDev) {
+        plugins.push(new webpack.HotModuleReplacementPlugin());
+        plugins.push(new ReactRefreshWebpackPlugin());
+        // plugins.push(new BundleAnalyzerPlugin({
+        //     openAnalyzer: false,
+        // }));
+    }
+
+    return plugins;
+};
