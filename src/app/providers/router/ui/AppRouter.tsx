203680781343d541d2534863c9f3d473612381c2
s.markov <s.markov@vipservice.ru>
2023-03-17 09:42:54 +0300

    7.47 Router v6 private protectеd routes. Защищенные маршруты

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,35 +1,32 @@
 import { Routes, Route } from 'react-router-dom';
-import { Suspense, useMemo } from 'react';
-import { routeConfig } from 'shared/config/routeConfig/routeConfig';
+import { Suspense, useCallback } from 'react';
+import { AppRoutesProps, routeConfig } from 'shared/config/routeConfig/routeConfig';
 import { PageLoader } from 'widgets/PageLoader';
-import { getUserAuthData } from 'entities/User';
-import { useSelector } from 'react-redux';
+import { RequireAuth } from './RequireAuth';
 
 const AppRouter = () => {
-    const isAuth = useSelector(getUserAuthData);
+    const renderWithWrapper = useCallback((route: AppRoutesProps) => {
+        const element = (
+            <Suspense fallback={<PageLoader />}>
+                <div className="page-wrapper">
+                    {route.element}
+                </div>
+            </Suspense>
+        );
 
-    const routes = useMemo(() => Object.values(routeConfig).filter(((route) => {
-        if (route.authOnly && !isAuth) {
-            return false;
-        }
-
-        return true;
-    })), [isAuth]);
+        return (
+            <Route
+                key={route.path}
+                path={route.path}
+                element={route.authOnly ? <RequireAuth>{element}</RequireAuth> : element}
+            />
+        );
+    }, []);
 
     return (
         <Suspense fallback={<PageLoader />}>
             <Routes>
-                {routes.map(({ element, path }) => (
-                    <Route
-                        key={path}
-                        path={path}
-                        element={(
-                            <div className="page-wrapper">
-                                {element}
-                            </div>
-                        )}
-                    />
-                ))}
+                {Object.values(routeConfig).map(renderWithWrapper)}
             </Routes>
         </Suspense>
     );
--- src/entities/User/index.ts
+++ src/entities/User/index.ts
@@ -1,3 +1,4 @@
 export { userActions, userReducer } from './model/slice/userSlice';
 export { UserSchema, User } from './model/types/user';
+export { getUserInited } from './model/selectors/getUserInited/getUserInited';
 export { getUserAuthData } from './model/selectors/getUserAuthData/getUserAuthData';
