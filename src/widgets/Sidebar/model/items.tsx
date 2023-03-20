40312af2e54a1f05a6e0789d3f81c4ff3097c446
s.markov <s.markov@vipservice.ru>
2023-03-20 12:43:15 +0300

    7.48 ArticlesPage и ArticleDetailsPage. Декомпозиция. Сущности. Webpack publicPath

--- src/widgets/Sidebar/model/items.tsx
+++ src/widgets/Sidebar/model/items.tsx
@@ -2,6 +2,7 @@ import { RoutePath } from 'shared/config/routeConfig/routeConfig';
 import MainIcon from 'shared/assets/icons/main-20-20.svg';
 import AboutIcon from 'shared/assets/icons/about-20-20.svg';
 import ProfileIcon from 'shared/assets/icons/profile-20-20.svg';
+import ArticleIcon from 'shared/assets/icons/article-20-20.svg';
 import React from 'react';
 
 export interface SidebarItemType {
@@ -27,6 +28,12 @@ export const SidebarItemList: SidebarItemType[] = [
         path: RoutePath.profile,
         text: 'Профиль',
         Icon: ProfileIcon,
+        authOnly: true,
+    },
+    {
+        path: RoutePath.articles,
+        text: 'Статьи',
+        Icon: ArticleIcon,
         authOnly: true,
     },
 ];
