a61d441cf14a6cfa99b98edf8f6622ec798987f9
s.markov <s.markov@vipservice.ru>
2023-06-23 09:09:42 +0300

    11.74 Исправляем стили

--- src/entities/Article/ui/ArticleDetails/ArticleDetails.tsx
+++ src/entities/Article/ui/ArticleDetails/ArticleDetails.tsx
@@ -10,6 +10,7 @@ import Avatar from 'shared/ui/Avatar/Avatar';
 import EyeIcon from 'shared/assets/icons/eye-20-20.svg';
 import CalendarIcon from 'shared/assets/icons/calendar-20-20.svg';
 import Icon from 'shared/ui/Icon/Icon';
+import { VStack } from 'shared/ui/Stack';
 import { ArticleCodeBlockComponent } from '../ArticleCodeBlockComponent/ArticleCodeBlockComponent';
 import { ArticleImageBlockComponent } from '../ArticleImageBlockComponent/ArticleImageBlockComponent';
 import { ArticleTextBlockComponent } from '../ArticleTextBlockComponent/ArticleTextBlockComponent';
@@ -127,9 +128,9 @@ export const ArticleDetails = memo((props: ArticleDetailsProps) => {
 
     return (
         <DynamicModuleLoader reducers={reducers} removeAfterUnmount>
-            <div className={classNames(cls.ArticleDetails, {}, [className])}>
+            <VStack gap="16" max className={classNames(cls.ArticleDetails, {}, [className])}>
                 {content}
-            </div>
+            </VStack>
         </DynamicModuleLoader>
     );
 });
