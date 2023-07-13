9fe7daa6aed3a18c4da5259509c5627b75bf9083
s.markov <s.markov@vipservice.ru>
2023-07-13 11:15:41 +0300

    12.86 StarRating. Модульный подход. Оценка статьи и профиля

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,5 +1,6 @@
 import { useTranslation } from 'react-i18next';
 import { Page } from '@/widgets/Page/Page';
+import { Rating } from '@/entities/Rating';
 
 const MainPage = () => {
     const { t } = useTranslation();
@@ -7,6 +8,7 @@ const MainPage = () => {
     return (
         <Page>
             {t('Главная страница')}
+            <Rating hasFeedback />
         </Page>
     );
 };
